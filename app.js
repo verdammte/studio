b080d366a75f43485da62d0ab3d7212853aded9a
Dero Kratzberg <bonsai.jem@gmail.com>
2017-06-24 00:46:37 -0600

    Server setup in app,js

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(req.url);
-    res.end();
-}).listen(8080);
+app.set('port', process.env.PORT || 8000);
+app.use(serveStatic(__dirname));
+app.listen(8000, function(err) {
+    if (err) {
+        throw err;
+    }
+    console.log('Express server listening on port 8000');
+});
+
+//set up app
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json({
+    type: 'application/*+json'
+}));
+
+app.use(function(req, res, next) {
+    //create global full-access connection on successful connection -- fallback for issue where connection is killed on error
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
