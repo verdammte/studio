27e1479a601c87563e2d0bf094980a348efb9e57
Dero Kratzberg <bonsai.jem@gmail.com>
2017-06-23 22:47:38 -0600

    Added app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(req.url);
+    res.end();
+}).listen(8080);
